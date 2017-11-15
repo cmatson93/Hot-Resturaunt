187105b55decfda5396a5bc0b3e8b2c8d8b91ff1
Christina  Matson <christinamatson@Christinas-MacBook-Pro.local>
2017-11-15 12:34:20 -0800

    Path Fixed

--- data/waitlist.js
+++ data/waitlist.js
@@ -1,25 +1,25 @@
 var waitingListArray = [
-  {
-    routeName: "Yoda",
-    name: "Yoda",
-    phone: "123-123-1234",
-    email: "christinaEmail",
-    uniqueId: "#1"
-  },
-  {
-    routeName: "Luke",
-    name: "Luke",
-    phone: "123-123-1234",
-    email: "jpemail",
-    uniqueId: "#2"
-  },
-  {
-    routeName: "Han Solo",
-    name: "Han Solo",
-    phone: "123-123-1234",
-    email: "nelsonemail",
-    uniqueId: "#3"
-  }
+  // {
+  //   routeName: "Yoda",
+  //   name: "Yoda",
+  //   phone: "123-123-1234",
+  //   email: "christinaEmail",
+  //   uniqueId: "#1"
+  // },
+  // {
+  //   routeName: "Luke",
+  //   name: "Luke",
+  //   phone: "123-123-1234",
+  //   email: "jpemail",
+  //   uniqueId: "#2"
+  // },
+  // {
+  //   routeName: "Han Solo",
+  //   name: "Han Solo",
+  //   phone: "123-123-1234",
+  //   email: "nelsonemail",
+  //   uniqueId: "#3"
+  // }
 ];
 
 module.exports = waitingListArray;
